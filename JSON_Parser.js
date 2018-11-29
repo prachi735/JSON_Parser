eda4c68d879735b7a099f47fc324c666a80268f6
Prachi735 <Prachi735@gmail.com>
2018-11-29 17:21:05 +0530

    Addes test cases for all parsers

--- JSON_Parser.js
+++ JSON_Parser.js
@@ -1,22 +1,24 @@
 testParser()
 
 function testParser () {
-  let obj = `{"key1" : 1, "K2": 12.3 ,   "K3": -1.0 , "k6":1.3e2, "K8":0.0}`
-  console.log(parseValue(obj)[0])
+  let result = null
 
-  obj = `{"key" : "ab c", "k1":"12", "k2":123,    "k3":   12, "k4":2, "k5":[1,2, [3,4], {"J":1}] , "Obj": {"k6":5} }`
-  console.log(parseValue(obj)[0])
-
-  obj = `{"card":"2","numbers":{"Conway":[1,11,21,1211,111221,312211],"Fibonacci":[0,1,1,2,3,5,8,13,21,34]}}`
-  console.log(parseValue(obj))
-
-  obj = `{"ID":null,"name":"Doe","first-name":"John","age":25,"hobbies":["reading","cinema",{"sports":["volley-ball","snowboard"]}],"address":{}}`
-  console.log(parseValue(obj))
-
-  obj = `{noquotes:noquotes,"\\u must be followed by 4 digits":"\\u01A",unquoted property:unknown_value,"unescaped backslash \ ":"\ ","malformed object":{"1"}}`
-  console.log(parseValue(obj))
-
+  let testObjects = [ `{"key1" : 1, "K2": 12.3 ,   "K3": -1.0 , "k6":1.3e2, "K8":0.0}`,
+    `{"key" : "ab c", "k1":"12", "k2":123,    "k3":   12, "k4":2, "k5":[1,2, [3,4], {"J":1}] , "Obj": {"k6":5} }`,
+    `{"card":"2","numbers":{"Conway":[1,11,21,1211,111221,312211],"Fibonacci":[0,1,1,2,3,5,8,13,21,34]}}`,
+    `{"ID":null,"name":"Doe","first-name":"John","age":25,"hobbies":["reading","cinema",{"sports":["volley-ball","snowboard"]}],"address":{}}`,
+    `{noquotes:noquotes,"\\u must be followed by 4 digits":"\\u01A",unquoted property:unknown_value,"unescaped backslash \ ":"\ ","malformed object":{"1"}}`
+  ]
+  // let parser = new Parser()
+  for (let obj of testObjects) {
+    result = parseValue(obj)
+    if (result !== null) {
+      console.log(result[0])
+    } else console.log(`Invalid JSON`)
+  }
 }
+
+// function Parser () {
 
 function parseValue (input) {
   let result
@@ -130,13 +132,14 @@ function parseObject (input) {
   }
   input = input.trim()
   if (!input.startsWith('}')) return null
-  // console.log(input)
   return [obj, input.slice(1)]
 }
 
 function commaParser (input) {
   return input.startsWith(',') ? [',', input.slice(1)] : null
 }
+
 function colonParser (input) {
   return input.startsWith(':') ? [':', input.slice(1)] : null
 }
+// }
