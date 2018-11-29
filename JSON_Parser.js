7986f3dcd1893607635b58603b8a6acc5a114431
Prachi735 <Prachi735@gmail.com>
2018-11-29 08:58:04 +0530

    Added Object Parser

--- JSON_Parser.js
+++ JSON_Parser.js
@@ -13,23 +13,26 @@ function ParseBoolean (input) {
 }
 
 function ParseNumber (input) {
-  // console.log(input)
-  let re = /^-?[1-9]+[0-9]*$|^0$|^-?[0-9][1-9]*.[0-9]$|^-?[0-9][1-9]*.[0-9]+e-?[1-9]+/
-  let result = re.test(input)
+  let re = /^-?[1-9]+[0-9]*|^0|^-?[0-9][1-9]*.[0-9]|^-?[0-9][1-9]*.[0-9]+e-?[1-9]+/
+  let result = input.match(re)
   if (result) {
-    input = input.toString()
-    let eIndex = input.indexOf('e') + input.indexOf('E') + 1
+    let eIndex = result[0].indexOf('e') + result[0].indexOf('E') + 1
     let n = 0
     if (eIndex > 0) {
-      let base = Number(input.substr(0, eIndex))
-      let power = Number(input.substr(eIndex + 1))
+      let base = Number(result[0].substr(0, eIndex))
+      let power = Number(result[0].substr(eIndex + 1))
       n = (base * 10 ** power)
     } else {
-      n = Number(input)
+      // n = Number(result[0])
+      n = Number(result[0])
     }
-    let num = input.match(re)
-    return [n, input.slice(num.length)]
+    console.log(n, input)
+    return [n, input.slice(result[0].length)]
   } else return null
+
+//   let regexp = String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)
+//   if (!String(input).match(/^[-+]?(\d+(\.\d*)?|\.\d+)([e][+-]?\d+)?/)) return null
+// return [parseInt(regexp[0]), input.slice(regexp[0].length)]
 }
 
 function ParseString (input) {
@@ -65,127 +68,137 @@ function ParseArray (input) {
     while (true) {
       result = ParseSpace(input)
       if (result !== null) {
-        return null
+        input = result[1]
       }
-      input = result[1]
-      result = ParseToken(input)
+      result = ParseValue(input)
       if (result) {
         arr.push(result[0])
         input = result[1]
       } else break
       result = ParseSpace(input)
-      if (result == null) {
+      if (result !== null) {
+        input = result[1]
       }
-      input = result[1]
       result = ParseComma(input)
-      if (result == null) {
-        return null
+      if (result !== null) {
+        input = result[1]
       }
-      input = result[1]
       if (input.charAt('0') === ']') {
-        return null
+        input = input.slice(']')
+        break
       }
     }
-    return arr
-  } else return null
+    return [arr, input]
+  }
+  return null
 }
 
 function ParseObject (input) {
   let obj = {}
   let key = ''
-  // let value = null
   let result = null
 
   if (input.charAt(0) === '{') {
+    input = input.slice(1)
     while (true) {
+      console.log('0', input)
       result = ParseSpace(input)
-      if (result == null) {
-        return null
+      if (result !== null) {
+        input = result[1]
       }
       result = ParseString(input)
       if (result === null) {
-        return null
+        break
       }
       key = result[0]
       input = result[1]
-      input = ParseSpace(input)
-      if (result == null) {
-        return null
+      result = ParseSpace(input)
+      if (result !== null) {
+        input = result[1]
       }
-      input = ParseColon(input)
-      if (result == null) {
-        return null
-      }
-      input = result[1]
-      input = ParseSpace(input)
-      if (result == null) {
-        return null
-      }
-      input = result[1]
-      input = ParseToken(input)
-      if (result == null) {
-        return null
-      }
-      input = result[0]
-      obj[key] = result[0]
-
-      result = ParseToken(input)
-      if (result === null) {
-        return null
+      result = ParseColon(input)
+      if (result !== null) {
+        input = result[1]
       }
       result = ParseSpace(input)
-      if (result == null) {
-        return null
+      if (result !== null) {
+        input = result[1]
       }
-      input = result[1]
+      result = ParseValue(input)
+      console.log('i0', input, result)
+      if (result !== null) {
+        obj[key] = result[0]
+        input = result[1]
+        console.log(input)
+      } else {
+        console.log('i1', input)
+        break
+      }
+      result = ParseSpace(input)
+      console.log('1', input)
+      if (result !== null) {
+        input = result[1]
+      }
+      console.log('2', input)
       result = ParseComma(input)
-      if (result == null) {
-        return null
+      if (result !== null) {
+        input = result[1]
       }
-      input = result[1]
+      console.log('3', input)
       if (input.charAt('0') === '}') {
-        return null
+        console.log('i2', input)
+        break
       }
-      return obj
+      console.log('4', input)
     }
-  } else return null
+    return [obj, input]
+  }
+  return null
 }
 
 function Parse () {
-  let obj = `{"key" : "ab c", "k":"12", "k":123, "k":[1,2,3,4], "k":1e2, "":2 }`
-  console.log(ParseObject(obj))
+  let obj = `{"key1" : 1 , "Key2": 2, "Key3": 3 }`
+  // console.log(ParseObject(obj)[0])
 
-  obj = `{"card":"2","numbers":{"Conway":[1,11,21,1211,111221,312211],"Fibonacci":[0,1,1,2,3,5,8,13,21,34]}}`
-  console.log(ParseObject(obj))
+  obj = `{"key" : "ab c", "k1":"12", "k2":123, "k3":12, "k4":2, "k5":[1,2] "Obj": {"k6":5} }`
+  console.log(ParseObject(obj)[0])
 
-  obj = `{"ID":null,"name":"Doe","first-name":"John","age":25,"hobbies":["reading","cinema",{"sports":["volley-ball","snowboard"]}],"address":{}}`
-  console.log(ParseObject(obj))
+  // obj = `{"card":"2","numbers":{"Conway":[1,11,21,1211,111221,312211],"Fibonacci":[0,1,1,2,3,5,8,13,21,34]}}`
+  // console.log(ParseObject(obj))
 
-  obj = `{noquotes:noquotes,"\\u must be followed by 4 digits":"\\u01A",unquoted property:unknown_value,"unescaped backslash \ ":"\ ","malformed object":{"1"}}`
-  console.log(ParseObject(obj))
+  // obj = `{"ID":null,"name":"Doe","first-name":"John","age":25,"hobbies":["reading","cinema",{"sports":["volley-ball","snowboard"]}],"address":{}}`
+  // console.log(ParseObject(obj))
+
+  // obj = `{noquotes:noquotes,"\\u must be followed by 4 digits":"\\u01A",unquoted property:unknown_value,"unescaped backslash \ ":"\ ","malformed object":{"1"}}`
+  // console.log(ParseObject(obj))
 }
 
 Parse()
 
 // Helper functions
+// function ParseCharacter (input, ch) {
+//   console.log('Prase Char',input, ch, ch === ' ')
+//   if (ch === ' ') {
+//     console.log(input.match(/\S/))
+//     if (input.match(/\S/)) {
+//       return [null, input.slice(input.match(/\S/))]
+//     } else return null
+//   }
+//   if (input.startsWith(ch)) {
+//     return [ch, input.slice(1)]
+//   } else return null
+// }
+
 function ParseComma (input) {
-  if (input.startsWith(',')) {
-    return [',', input.slice(1)]
-  } else return null
+  return input.startsWith(',') ? [',', input.slice(1)] : null
 }
-
 function ParseColon (input) {
-  if (input.startsWith(':')) {
-    return [':', input.slice(1)]
-  } else return null
+  return input.startsWith(':') ? [':', input.slice(1)] : null
 }
-
 function ParseSpace (input) {
-  if (input.match(' ')) {
-    return [null, input.slice(input.match(' ').index)]
-  } else return null
+  return input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
 }
 
-function ParseToken (input) {
+function ParseValue (input) {
   return ParseObject(input) || ParseArray(input) || ParseString(input) || ParseNumber(input) || ParseNull(input) || ParseBoolean(input)
 }
